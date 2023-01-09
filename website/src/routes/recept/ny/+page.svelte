61ca135e683f208f4ca08ce205170fe9cbb86ba0
Alexander Holm <alho0014@student.hv.se>
2023-01-09 22:13:40 +0100

    Sida för att lägga till recept

--- website/src/routes/recept/ny/+page.svelte
+++ website/src/routes/recept/ny/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
     import { page } from "$app/stores";
+    import Header from "src/components/Header.svelte";
     import { Days } from "src/models/Week";
+    import { fade } from "svelte/transition";
 
     const weekId = $page.url.searchParams.get("vecka");
-    const nameOfDays = Object.values(Days).filter(val => isNaN(Number(val))) as string[];
     /*
         Typescript enums har formatet:
         0: Array [ "0", "Måndag" ]​
@@ -20,26 +21,84 @@
     const days = stupidTypescriptEnum.slice(stupidTypescriptEnum.length /2);
 </script>
 
-<form method="post" >
-    <h1>Nytt recept</h1>
+<Header backUrl="/" />
+<form method="post" in:fade >
+    <h2>Lägg till recept</h2>
     <input type="hidden" name="week" value={weekId} />
+
     <label for="url">Länk</label>
     <input id="url" name="url" type="text" required />
-    <label for="notes"></label>
+
+    <label for="notes">Anteckningar</label>
     <textarea id="notes" name="notes" />
-    <p>Dag</p>
-    {#each days as [name, number]}
-        <div class="day">
-            <label for={name}>{name}</label>
+
+    <fieldset class="days">
+        <legend>Dag</legend>
+        {#each days as [name, number]}
             <input id={name} name="day" value={number} type="radio" required/>
-        </div>
-    {/each}
-    <div class="links" >
-        <a href="/" >Tillbaka</a>
-        <button type="submit" >Spara</button>
-    </div>
+            <label for={name} class="day-button">{name}</label>
+        {/each}
+
+    </fieldset>
+
+    <button type="submit" class="button-primary" >Spara</button>
 </form>
 
 <style>
+    form{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+    }
+    label{
+        margin-top: 40px;
+    }
+    #url, #notes, .days{
+        border: 1px solid lightgray;
+        border-radius: var(--border-radius);
+        box-sizing: border-box;
+    }
+
+    #url, #notes{
+        font-family: inherit;
+        color: var(--black);
+        width: 40rem;
+        max-width: 100%;
+        margin: auto;
+        padding: 8px;        
+    }
+
+
+    .days{
+        width: 100%;
+        margin-top: 40px;
+        margin-bottom: 80px;
+        padding: 20px;
+    }
+    legend{
+        margin: auto;
+    }
     
+    .day-button{
+        display: inline-block;
+        background-color: transparent;
+        color: var(--black);
+        border: 2px solid;
+        outline: 2px solid;
+        outline-offset: -2px;
+        border-radius: 4px;
+        font-size: 1rem;
+        padding: 12px;
+        margin: 20px;
+
+        transition: outline-offset 100ms ease-out;
+    }
+    input[type="radio"]{
+        display: none;
+    }
+    input[type="radio"]:checked + .day-button{
+        outline-offset: -8px;
+    }
+
 </style>
