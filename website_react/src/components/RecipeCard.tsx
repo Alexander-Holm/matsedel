6b4afda41172e4ce69735b9ab56225485e6a1024
Alexander Holm <alho0014@student.hv.se>
2023-02-14 16:37:06 +0100

    Lagt till en standardbild för linkPreview

--- website_react/src/components/RecipeCard.tsx
+++ website_react/src/components/RecipeCard.tsx
@@ -2,19 +2,32 @@ import { Link } from "react-router-dom";
 import { Recipe } from "../models/Recipe";
 import styles from "./RecipeCard.module.css";
 import { ReactComponent as NotesIcon } from "../icons/message.svg";
+import { SyntheticEvent } from "react";
 
+const defaultImage = "/images/default-recipe-image.svg"
 interface props{
     recipe: Recipe,
 }
 export default function RecipeCard( props: props ){
     const { id, linkPreview, notes } = props.recipe; 
 
+    function onImageError(event: SyntheticEvent<HTMLImageElement>){
+        const image = event.currentTarget;
+        // Förhindra oändlig loop om det skulle vara något fel på default-bilden
+        image.onerror = null; 
+        image.src = defaultImage;
+        // Uppdaterar linkPreview för att få en bild på detaljsidan.
+        // Man bör egentligen inte modifiera props men det funkar här.
+        if(props.recipe.linkPreview)
+            props.recipe.linkPreview.imageUrl = defaultImage;
+    }
+
     if(linkPreview != null){
         const { title, description, imageUrl } = linkPreview;
         return(
             <div className={styles.recipePreview}>    
                 <Link to={`/recept/${id}`} state={props.recipe} className={styles.card} >
-                    <img src={imageUrl} alt="" className={styles.image} />
+                    <img src={imageUrl} alt="" className={styles.image} onError={onImageError} />
                     <div className={styles.textContainer}>
                         <h4 className={styles.title}>{title}</h4>
                         <p className={styles.description}>{description}</p>
